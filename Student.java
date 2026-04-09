89b0bbe55ff3d7a06023c41a5b5c5e957d521ccf
Sarvesh <sarveshch2004@gmail.com>
2026-04-09 12:52:44 +0530

    Final Commit

---
+++ Student.java
@@ -0,0 +1,11 @@
+public class Student {
+    int rollNo;
+    String name;
+    double marks;
+
+    public Student(int rollNo, String name, double marks){
+        this.rollNo = rollNo;
+        this.name = name;
+        this.marks = marks;
+    }
+}   
